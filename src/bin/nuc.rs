60fe183a50cfea1c3ee51fde1c1f1ca9840dc1cc
Michael Daffin <michael@daffin.io>
2018-01-14 15:55:35 +0000

    Basic functionality implemented

--- src/bin/nuc.rs
+++ src/bin/nuc.rs
@@ -17,10 +17,17 @@ struct Opt {
 fn main() {
     let opt = Opt::from_args();
     for arg in opt.input {
-        let num: Number = arg.parse().expect("failed to parse input");
-        print!("{}\t", num);
-        print!("{:#x}\t", num);
-        print!("{:#b}", num);
+        let num: Number = match arg.parse() {
+            Ok(i) => i,
+            Err(e) => {
+                println!("Failed to parse input '{}': {}", arg, e);
+                std::process::exit(1);
+            }
+        };
+        print!("{:>8} ", num);
+        print!("{:>8} ", num.fmt_signed());
+        print!("{:>#8x} ", num);
+        print!("{:>#16b}", num);
         println!();
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,19 +8,52 @@ mod error;
 pub use error::ParseNumberError;
 
 pub enum Number {
+    U4(u8),
     U8(u8),
     U16(u16),
     U32(u32),
-    I8(i8),
-    I16(i16),
-    I32(i32),
+    U64(u64),
+}
+
+impl Number {
+    pub fn fmt_signed(&self) -> String {
+        use Number::*;
+        match *self {
+            U4(i) => format!("{}", i as i8),
+            U8(i) => format!("{}", i as i8),
+            U16(i) => format!("{}", i as i16),
+            U32(i) => format!("{}", i as i32),
+            U64(i) => format!("{}", i as i64),
+        }
+    }
 }
 
 impl FromStr for Number {
     type Err = ParseNumberError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Number::I8(s.parse()?))
+        Ok(match s {
+            s if s.starts_with("0x") => {
+                match s.len() {
+                    0...3 => Number::U4(u8::from_str_radix(&s[2..], 16)?),
+                    3...4 => Number::U8(u8::from_str_radix(&s[2..], 16)?),
+                    5...6 => Number::U16(u16::from_str_radix(&s[2..], 16)?),
+                    6...10 => Number::U32(u32::from_str_radix(&s[2..], 16)?),
+                    _ => Number::U64(u64::from_str_radix(&s[2..], 16)?),
+                }
+            }
+            s if s.starts_with("0b") => {
+                match s.len() {
+                    0...6 => Number::U4(u8::from_str_radix(&s[2..], 2)?),
+                    7...10 => Number::U8(u8::from_str_radix(&s[2..], 2)?),
+                    11...18 => Number::U16(u16::from_str_radix(&s[2..], 2)?),
+                    19...34 => Number::U32(u32::from_str_radix(&s[2..], 2)?),
+                    _ => Number::U64(u64::from_str_radix(&s[2..], 2)?),
+                }
+            }
+            s if s.starts_with("-") => Number::U64(s.parse::<i64>()? as u64),
+            s => Number::U64(s.parse()?),
+        })
     }
 }
 
@@ -29,12 +62,11 @@ impl fmt::Display for Number {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         use Number::*;
         match *self {
-            U8(i) => write!(f, "{}", i),
-            U16(i) => write!(f, "{}", i),
-            U32(i) => write!(f, "{}", i),
-            I8(i) => write!(f, "{}", i),
-            I16(i) => write!(f, "{}", i),
-            I32(i) => write!(f, "{}", i),
+            U4(i) => f.pad(&format!("{}", i)),
+            U8(i) => f.pad(&format!("{}", i)),
+            U16(i) => f.pad(&format!("{}", i)),
+            U32(i) => f.pad(&format!("{}", i)),
+            U64(i) => f.pad(&format!("{}", i)),
         }
     }
 }
@@ -43,12 +75,11 @@ impl fmt::Binary for Number {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         use Number::*;
         match *self {
-            U8(i) => write!(f, "{:#010b}", i),
-            U16(i) => write!(f, "{:#018b}", i),
-            U32(i) => write!(f, "{:#034b}", i),
-            I8(i) => write!(f, "{:#010b}", i),
-            I16(i) => write!(f, "{:#018b}", i),
-            I32(i) => write!(f, "{:#034b}", i),
+            U4(i) => f.pad(&format!("{:#006b}", i)),
+            U8(i) => f.pad(&format!("{:#010b}", i)),
+            U16(i) => f.pad(&format!("{:#018b}", i)),
+            U32(i) => f.pad(&format!("{:#034b}", i)),
+            U64(i) => f.pad(&format!("{:#066b}", i)),
         }
     }
 }
@@ -58,12 +89,11 @@ impl fmt::UpperHex for Number {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         use Number::*;
         match *self {
-            U8(i) => write!(f, "{:#04X}", i),
-            U16(i) => write!(f, "{:#06X}", i),
-            U32(i) => write!(f, "{:#010X}", i),
-            I8(i) => write!(f, "{:#04X}", i),
-            I16(i) => write!(f, "{:#06X}", i),
-            I32(i) => write!(f, "{:#010X}", i),
+            U4(i) => f.pad(&format!("{:#03X}", i)),
+            U8(i) => f.pad(&format!("{:#04X}", i)),
+            U16(i) => f.pad(&format!("{:#06X}", i)),
+            U32(i) => f.pad(&format!("{:#010X}", i)),
+            U64(i) => f.pad(&format!("{:#018X}", i)),
         }
     }
 }
@@ -72,12 +102,11 @@ impl fmt::LowerHex for Number {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         use Number::*;
         match *self {
-            U8(i) => write!(f, "0x{:04x}", i),
-            U16(i) => write!(f, "0x{:06x}", i),
-            U32(i) => write!(f, "0x{:010x}", i),
-            I8(i) => write!(f, "0x{:04x}", i),
-            I16(i) => write!(f, "0x{:06x}", i),
-            I32(i) => write!(f, "0x{:010x}", i),
+            U4(i) => f.pad(&format!("{:#03x}", i)),
+            U8(i) => f.pad(&format!("{:#04x}", i)),
+            U16(i) => f.pad(&format!("{:#06x}", i)),
+            U32(i) => f.pad(&format!("{:#010x}", i)),
+            U64(i) => f.pad(&format!("{:#018x}", i)),
         }
     }
 }
